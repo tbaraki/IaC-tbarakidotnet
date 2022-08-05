e070bd09ff0c9163b7c7495fe14a0dde061beae6
Travis Baraki <travis.baraki@gmail.com>
2022-08-05 09:01:44 -0700

    add variables.tf and update main.tf

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+  type    = string
+  default = "RG-tbarakidotnet"
+}
+
+variable "location" {
+  type    = string
+  default = "Central US"
+}
+
+variable "sku_tier" {
+  type    = string
+  default = "Free"
+}
