4af35c889dd35ef6207ee64185a6fab2252772bd
Travis Baraki <travis.baraki@gmail.com>
2022-08-05 11:54:11 -0700

    update resources names to match TF naming conventions.

--- main.tf
+++ main.tf
@@ -19,21 +19,21 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "RG-tbarakidotnet" {
+resource "azurerm_resource_group" "tbarakidotnet" {
   name     = var.resource_group_name
   location = var.location
 }
 
 resource "azurerm_static_site" "tbarakidotnet" {
   name                = "tbarakidotnet"
-  resource_group_name = azurerm_resource_group.RG-tbarakidotnet.name
+  resource_group_name = azurerm_resource_group.tbarakidotnet.name
   location            = var.location
   sku_tier            = var.sku_tier
 }
 
-resource "azurerm_static_site" "tbarakidotnet-resume" {
+resource "azurerm_static_site" "tbarakidotnet_resume" {
   name                = "tbarakidotnet-resume"
-  resource_group_name = azurerm_resource_group.RG-tbarakidotnet.name
+  resource_group_name = azurerm_resource_group.tbarakidotnet.name
   location            = var.location
   sku_tier            = var.sku_tier
 }
