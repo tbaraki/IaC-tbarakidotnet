e112468de5ee42c25d3d4f11c7284e31604ce81b
Travis Baraki <travis.baraki@gmail.com>
2022-08-15 11:43:54 -0700

    Add Cloudflare provider, define DNS records

--- main.tf
+++ main.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.16.0"
     }
+
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = ">=3.21.0"
+    }
   }
 
   cloud {
@@ -19,6 +24,22 @@ provider "azurerm" {
   features {}
 }
 
+provider "cloudflare" {
+  api_token = data.azurerm_key_vault_secret.cf_token.value
+}
+
+// Read Cloudflare API token from Azure Key Vault
+data "azurerm_key_vault" "key_vault" {
+  name                = "TGBProjectKeys"
+  resource_group_name = var.resource_group_name
+}
+
+data "azurerm_key_vault_secret" "cf_token" {
+  name         = "Cloudflare"
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+// Define static web appps in a resource group
 resource "azurerm_resource_group" "tbarakidotnet" {
   name     = var.resource_group_name
   location = var.location
@@ -36,4 +57,32 @@ resource "azurerm_static_site" "tbarakidotnet_resume" {
   resource_group_name = azurerm_resource_group.tbarakidotnet.name
   location            = var.location
   sku_tier            = var.sku_tier
+}
+
+// Define Cloudflare DNS records
+resource "cloudflare_record" "root" {
+  zone_id = var.tbarakidotnet_zone_id
+  name    = "tbaraki.net"
+  value   = resource.azurerm_static_site.tbarakidotnet.default_host_name
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+}
+
+resource "cloudflare_record" "www" {
+  zone_id = var.tbarakidotnet_zone_id
+  name    = "www"
+  value   = resource.azurerm_static_site.tbarakidotnet.default_host_name
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+}
+
+resource "cloudflare_record" "resume" {
+  zone_id = var.tbarakidotnet_zone_id
+  name    = "resume"
+  value   = resource.azurerm_static_site.tbarakidotnet_resume.default_host_name
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
 }
--- variables.tf
+++ variables.tf
@@ -12,3 +12,13 @@ variable "sku_tier" {
   type    = string
   default = "Free"
 }
+
+variable "tbarakidotnet_zone_id" {
+  type    = string
+  default = "83faeb416715ceadd9eda9feb9679915"
+}
+
+variable "cf_acc_id" {
+  type    = string
+  default = "cf730f668070aecc1ad6e97e9c7ac9e3"
+}
